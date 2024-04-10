ca3d6b5ab1ea1d21e8ec32ea801712cf008ef74e
Diogo <profissional.diogoribeiro@gmail.com>
2024-04-10 13:20:01 -0300

    Adding admin user

--- store/models.py
+++ store/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+# Categories of product
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    pass
